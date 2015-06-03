8bd3abe43d2c00705a50a257a6bf38384f81dc25
chalagashvili <ichal13@freeuni.edu.ge>
2015-06-03 15:31:55 +0400

    Test file

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by GM on 6/3/2015.
+ */
+public class Test extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        //aeee
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}

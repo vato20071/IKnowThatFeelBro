21688799203faa20b81461e8f30b91626847f885
marie25 <mari.bezhashvili@gmail.com>
2015-06-04 15:40:11 -0700

    Updated DataBaseInterface

--- src/core/DataBaseInterface.java
+++ src/core/DataBaseInterface.java
@@ -4,13 +4,12 @@ public interface DataBaseInterface {
 
 	public void insertDataIntoAccount(Account user);
 	public void insertDataIntoCategories(Category category);
-	public void insertDataIntoRooms(Room room);
-	public void addFriendShip(Account user1, Account user2);
+	public void addFriendShip(String user1, String user2, String catName);
 	
 	public Account getAccountByID(int ID);
 	public Account getAccountByName(String name);
 	public Category getCategoryByID(int ID);
-	public Room getRoomByID(int ID);
-	public Category getFriendShip(Account user1, Account user2);
+	public Category getCategoryByName(String name);
+	public boolean getFriendShip(Account user1, Account user2);
 	
 }

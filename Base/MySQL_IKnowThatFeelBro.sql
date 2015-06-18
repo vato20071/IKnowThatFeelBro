2eba75e2383005caa3fbf7139bcc684ab2a221f4
vato20071 <vtaba13@freeuni.edu.ge>
2015-06-18 13:02:38 +0400

    Changed Admin status to int in DataBase

--- Base/MySQL_IKnowThatFeelBro.sql
+++ Base/MySQL_IKnowThatFeelBro.sql
@@ -3,18 +3,21 @@ create database if not exists IKnowThatFeelBro;
 
 use IKnowThatFeelBro;
 
+select * from account;
+select * from category;
+update account set `status` = 3 where user_name = 'vato20071';
 create table if not exists `account`
 (
 	`ID` int not null auto_increment primary key,
 	`user_name` varchar(100) not null,
 	`password` varchar(200) not null,
-	`nickname` varchar(100) not null,
+    `nickname` varchar(100) not null,
 	`mail` varchar(150) default null,
 	`fb` varchar(150) default null,
 	`gplus` varchar(150) default null,
 	`coeff_value` decimal(3, 2) default 0,
 	`coeff_numb` int default 0,
-	`status` enum('0', '1', '2') default 2, #0-admin, 1-moderator, 2-user
+	`status` int default 0, #1-admin, 2-banned, all other - user
 	unique (`user_name`)
 );
 
@@ -35,5 +38,3 @@ create table if not exists `friendship`
 	foreign key (`user2_ID`) references `account`(`ID`) on delete cascade,
 	foreign key (`cat_ID`) references `category`(`ID`) on delete cascade
 );
-
-
--- src/servlets/AdminLogin.java
+++ src/servlets/AdminLogin.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,7 +20,7 @@ import core.Server;
 @WebServlet("/AdminLogin")
 public class AdminLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final int ADMIN_NUM = 1;
+	private static final int ADMIN_NUM = 2;
 	private static final int TOKEN = 121314;
 
 	/**
@@ -56,19 +58,29 @@ public class AdminLogin extends HttpServlet {
 		int online = serv.getCountActiveUsers();
 		if (acc != null && db.getAccountByName(name).getStatus() == ADMIN_NUM) {
 			String offeredPassword = request.getParameter("password");
-			String realPassword = acc.getPassword();
+			MessageDigest m;
 			try {
-				if(offeredPassword.equals(realPassword)) {
-					request.getSession().setAttribute("adminLet", TOKEN);
-					request.getSession().setAttribute("amount", amount);
-					request.getSession().setAttribute("online", online);
-					request.getSession().setAttribute("alreadyLogged", "universal");
-					response.sendRedirect("adminLoggedIn.jsp");
-				} else {
-					response.sendRedirect("adminLogError.jsp");
+				m = MessageDigest.getInstance("SHA");
+				offeredPassword = DataBase.hexToString(m.digest(offeredPassword
+						.getBytes()));
+				String realPassword = acc.getPassword();
+				try {
+					if (offeredPassword.equals(realPassword)) {
+						request.getSession().setAttribute("adminLet", TOKEN);
+						request.getSession().setAttribute("amount", amount);
+						request.getSession().setAttribute("online", online);
+						request.getSession().setAttribute("alreadyLogged",
+								"universal");
+						response.sendRedirect("adminLoggedIn.jsp");
+					} else {
+						response.sendRedirect("adminLogError.jsp");
+					}
+				} catch (Exception ex) {
+
 				}
-			} catch(Exception ex){
-				
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}else {
 			response.sendRedirect("adminLogError.jsp");

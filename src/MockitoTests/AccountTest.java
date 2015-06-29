7c49220721a7405c43251129996f17ac061d70f8
Nuca <nnazg13@freeuni.edu.ge>
2015-06-29 18:00:36 +0400

    Test for Account

--- src/MockitoTests/AccountTest.java
+++ src/MockitoTests/AccountTest.java
@@ -1,14 +1,76 @@
 package MockitoTests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import com.google.gson.Gson;
+
+import core.Account;
+import core.DataBase;
 
 public class AccountTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Mock
+	Account acc;
+	
+	@Mock
+	DataBase base;
+	
+	@Mock
+	List<String> friends;
+	
+	@Mock
+	HashMap<String, List<String> > friendMap;
+	
+	
+	@Before
+	public void initMocks() {
+		acc = mock(Account.class);	
+		base = mock(DataBase.class);
+		friends = new ArrayList<>();
+		friendMap = new HashMap<>();
+		friendMap.put("cat", friends);
 	}
-
+	
+	@Test
+	public void testContainsFriend() {
+		when(acc.containsFriend("nuca")).thenReturn(true);
+		boolean res = acc.containsFriend("nuca");
+		assertTrue(res);
+	}
+	
+	@Test	
+	public void testAddFriendShipFriendIsFirst() {//String category, String userName
+		
+		base.addFriendShip("nuca", "vato", "cat");
+		friends = friendMap.get("cat");
+		
+		if (!friends.contains("vato")) { //we know there isn't "nuca", so this 'if' is used
+			friends.add("vato");
+			assertTrue(friends.contains("vato"));
+		}
+		
+		friendMap.put("cat", friends);
+		assertTrue(friends.equals(friendMap.get("cat")));
+	}
+	
+	@Test	
+	public void testAddFriendShipFriendIsNotFirst() {
+		
+		base.addFriendShip("nuca", "vato", "cat");
+		friends = friendMap.get("cat");
+		friends.add("vato");
+		
+		friendMap.put("cat", friends);
+		assertTrue(friends.equals(friendMap.get("cat")));
+	}
+	
 }

3c3a97be7255698a6c0dc9d67c0240c9c94cf73e
Nuca <nnazg13@freeuni.edu.ge>
2015-06-29 18:11:51 +0400

    Updated account and Mock Tests for it

--- src/core/Account.java
+++ src/core/Account.java
@@ -44,10 +44,6 @@ public class Account {
 	public void setFriendMap(HashMap<String, List<String>> friendMap) {
 		this.friendMap = friendMap;
 	}
-
-	public void test() {
-		System.out.println("HEre guys ");
-	}
 	
 	public void addFriendShip(String category, String userName) {
 		base.addFriendShip(this.userName, userName, category);
